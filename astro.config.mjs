4124d8d2d75785df43a4f2d9476bc2eaaf3188b8
divyesh-dbc <divyesh.dbc@gmail.com>
2026-02-03 16:02:10 +0530

    chore: Switch Astro adapter from Node to Vercel and update dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -10,9 +11,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://reactify-ai-astro.vercel.app",
   output: "server",
-  adapter: node({
+  /* adapter: node({
     mode: "standalone",
-  }),
+  }), */
+  adapter: vercel(),
   integrations: [react(), sitemap()],
   image: {
     domains: ["cdn.sanity.io", "images.unsplash.com", "plus.unsplash.com"],
